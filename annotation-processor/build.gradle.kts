ee649748d94bc1147026a829b050a44f561fb548
Igor Budzhak <z3rgatul@gmail.com>
2024-06-05 02:14:08 +0300

    AP fixes

--- annotation-processor/build.gradle.kts
+++ annotation-processor/build.gradle.kts
@@ -9,6 +9,8 @@ plugins {
     java
 }
 
+java.toolchain.languageVersion = JavaLanguageVersion.of(21)
+
 repositories {
     maven("https://repo.spongepowered.org/maven")
     mavenCentral()
@@ -18,6 +20,11 @@ dependencies {
     implementation("org.ow2.asm:asm:9.6")
     implementation("org.ow2.asm:asm-tree:9.6")
     implementation("org.spongepowered:mixin:0.8.5")
+}
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = "21"
+    targetCompatibility = "21"
 }
 
 sourceSets.main {
